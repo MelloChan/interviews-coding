d458bb0ccefb451b25922a5a11eca12c474cb75a
MelloYuki <352983198@qq.com>
2017-10-17 13:19:26 +0800

    从尾到头打印单链表

---
+++ No04.java
@@ -0,0 +1,39 @@
+package 剑指offer;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * 输入一个链表，从尾到头打印链表每个节点的值。
+ * Created by MelloChan on 2017/10/17.
+ */
+public class No04 {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(12);
+        ListNode l3 = new ListNode(31);
+        ListNode l4 = new ListNode(21);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        System.out.println(new Solution04().printListFromTailToHead(l1));
+    }
+}
+
+/**
+ * 解决思路:利用栈[先进后出]
+ */
+class Solution04 {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Stack<ListNode> stack = new Stack<>();
+        while (listNode != null) {
+            stack.push(listNode);
+            listNode = listNode.next;
+        }
+        while (!stack.empty()) {
+            arrayList.add(stack.pop().val);
+        }
+        return arrayList;
+    }
+}
