ceb65c417a01ce480c76dffef862a496696d16b5
MelloYuki <352983198@qq.com>
2017-10-17 16:19:25 +0800

    update

---
+++ ListNode.java
@@ -0,0 +1,14 @@
+package 剑指offer;
+
+/**
+ * 链表
+ * Created by MelloChan on 2017/10/17.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ TreeNode.java
@@ -0,0 +1,15 @@
+package 剑指offer;
+
+/**
+ * 构建二叉树
+ * Created by MelloChan on 2017/10/17.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
