30d16de2e3ef0d44f48d6d5a6060c732a283dd72
MelloYuki <352983198@qq.com>
2017-11-02 23:07:37 +0800

    update

---
+++ RandomListNode.java
@@ -0,0 +1,15 @@
+package 剑指offer;
+
+/**
+ * 复杂链表
+ * Created by MelloChan on 2017/11/2.
+ */
+public class RandomListNode {
+    private int label;
+    private RandomListNode next = null;
+    private RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
