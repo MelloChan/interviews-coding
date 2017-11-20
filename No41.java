1daee1a851a212b0a5e225989452e59071fa0b6c
MelloYuki <352983198@qq.com>
2017-11-20 19:59:47 +0800

    update 输入一个递增数组和一个数字sum,在数组中查找两个数字,其值等于sum

--- No41.java
+++ No41.java
@@ -14,11 +14,13 @@ public class No41 {
         int[] array = {1, 2, 4, 7, 11, 15};
         Solution41 solution41 = new Solution41();
         System.out.println(solution41.findNumbersWithSum(array, 15));
+        System.out.println(solution41.findNumbersWithSum(array, 7));
     }
 }
 
 /**
  * 解决方案:双指针
+ * 设置首尾双指针 相加 若小于sum 则首首指针++,大于sum 则尾指针--
  */
 class Solution41 {
     public ArrayList<Integer> findNumbersWithSum(int[] array, int sum) {
